e75c8f6d4c09e1b7ee114410e92ad85b39b581c5
Logan Chaffee <loganschaffee@gmail.com>
2022-05-20 17:24:32 -0700

    update reading speed test results css

--- client/src/components/Challenge/ReadingSpeedTest/ReadingSpeedTestResults/ReadingSpeedTestResults.js
+++ client/src/components/Challenge/ReadingSpeedTest/ReadingSpeedTestResults/ReadingSpeedTestResults.js
@@ -2,15 +2,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/esm/Row';
-import Col from 'react-bootstrap/esm/Col';
-import Button from 'react-bootstrap/esm/Button';
-
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { updateUser } from '../../../../actions/auth'
+import './ReadingSpeedTestResults.css'
 
-const ReadingSpeedTestCompletion = () => {
+const ReadingSpeedTestResults = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const user = useSelector((state) => state.auth)
@@ -25,33 +21,28 @@ const ReadingSpeedTestCompletion = () => {
         }
     }
 
-    const handleDiscard = () => {
-        navigate('/challenge')
-    }
+    const handleDiscard = () => navigate('/challenge')
 
-    const handleRestart = () => {
-        navigate('/reading-speed-test')
-    }
+    const handleRestart = () => navigate('/reading-speed-test')
 
-    
     return (
-        <Container className='ReadingSpeedCompletion' style={{margin: 'auto 0'}}>
+        <div className='ReadingSpeedTestResults' >
             <Row>
                 <Col xs={12}>
-                    <h3 className='text-center'>Your words per minute was:</h3>
-                    <h3 className='text-center'>{wpm}</h3>
+                    <h3 className='text-center'>Your Score:</h3>
+                    <h4 className='text-center'>{wpm} Words Per Minute</h4>
                     {wpm < user.wordsPerMinute && <p style={{textAlign:'center'}}>Your personal best is {user.wordsPerMinute}.<br />Are you sure you wish to overwrite your record?</p>}
                 </Col>
             </Row>
             <Row>
                 <Col xs={12}>
-                    <Button variant="outline-primary" className="full-width-btn" onClick={handleSave}>Save Results to Profile</Button>
-                    <Button variant="outline-danger" className="full-width-btn" onClick={handleDiscard}>Discard Results</Button>
-                    <Button variant="outline-secondary" className="full-width-btn" onClick={handleRestart}>Retake Test</Button>
+                    <Button variant="primary" className="full-width-btn" onClick={handleSave}>Save Results to Profile</Button>
+                    <Button variant="secondary" className="full-width-btn" onClick={handleRestart}>Retake Test</Button>
+                    <Button variant="danger" className="full-width-btn" onClick={handleDiscard}>Discard Results</Button>
                 </Col>
             </Row>
-        </Container>
+        </div>
     );
 }
 
-export default ReadingSpeedTestCompletion;
+export default ReadingSpeedTestResults;
