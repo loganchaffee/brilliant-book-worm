2c239b15a71c8aed1301cd6839624c06bac7d798
Logan Chaffee <loganschaffee@gmail.com>
2022-06-11 18:37:21 -0700

    Show badge for level 4 users

--- client/src/components/CurrentlyReading/LevelUp/LevelUp.js
+++ client/src/components/CurrentlyReading/LevelUp/LevelUp.js
@@ -1,6 +1,8 @@
 import React, { useState, useLayoutEffect, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Card, ProgressBar}  from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faCrown } from '@fortawesome/free-solid-svg-icons'
 import './LevelUp.css'
 
 const LevelUp = () => {
@@ -22,8 +24,6 @@ const LevelUp = () => {
         }
     }, [user.points])
 
-    console.log();
-
     return <div className='Dashboard__level Dashboard__card'>
         <div>
             <h4 className='Dashboard__level__level'>Level Up</h4>
@@ -36,11 +36,21 @@ const LevelUp = () => {
             <span>Total Points:</span>
             <span>{user.points}</span>
         </div>
-        <div className='d-flex justify-content-between'>
-            <span>Next Level At:</span>
-            <span>{nextLevelPoints} Points</span>
-        </div>
-        <ProgressBar variant='warning' now={((user.points - previousLevelPoints) / (nextLevelPoints - previousLevelPoints)) * 100} />
+        {
+            user.level < 4
+            ?
+            <>
+                <div className='d-flex justify-content-between'>
+                    <span>Next Level At:</span>
+                    <span>{nextLevelPoints} Points</span>
+                </div>
+                <ProgressBar variant='warning' now={((user.points - previousLevelPoints) / (nextLevelPoints - previousLevelPoints)) * 100} />
+            </>
+            :
+            <div className='level-4-badge'>
+                <FontAwesomeIcon icon={faCrown} /> Bibliophile
+            </div>
+        }
     </div>
 }
 
