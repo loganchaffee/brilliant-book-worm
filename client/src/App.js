1b3ed6f8fb10b0e9949f91b4f455151baa895575
Logan Chaffee <loganschaffee@gmail.com>
2022-09-06 18:29:52 -0700

    Refactor login and fetch all user information on login

--- client/src/App.js
+++ client/src/App.js
@@ -40,97 +40,76 @@ import NewUserWelcomeModal from './components/NewUserWelcomeModal/NewUserWelcome
 const App = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const params = useParams()
     const user = useSelector((state) => state.auth)
-    const posts = useSelector((state) => state.posts)
     const pointsJustScored = useSelector(state => state.pointsJustScored)
-    const currentPost = useSelector((state) => state.currentPost)
-    const currentVisitedUser = useSelector((state) => state.currentVisitedUser)
-    // const books = useSelector((state) => state.books)
-
-    // useEffect(() => {
-    //     let arr = []
-    //     for (let i = 0; i < books.length; i++) {
-    //         const book = books[i];
-    //         arr.push({ _id: book._id, thumbnail: book.thumbnail })
-    //     }
-    //     console.log(arr);
-    // }, [books])
-
 
     // The user data is stored in the redux store, but the boolean of isLoggedIn is stored here
     // In the main app component so that we can conditionally render the authentication form or the home page.
     const [isLoading, setIsLoading] = useState(true)
 
-    // Check for token in local storage and request user details
     useEffect(() => {
-        if (!user) {
-            if (localStorage.getItem('user')) {
-                dispatch(getUserInfo(navigate, setIsLoading))
-            } else {
-                setIsLoading(false)
-            }
-        } else {
+        setIsLoading(true)
+
+        if (localStorage.getItem('user')) {
+            console.log('There is a user, fetching all his shit');
+            dispatch(getUserInfo(navigate, setIsLoading))
+            dispatch(getBooks())
+            dispatch(getNotifications())
+        } 
+        
+        if (!localStorage.getItem('user')) {
+            console.log('There is no user. Show the landing page');
             setIsLoading(false)
         }
     }, [user?._id])
 
-    useEffect(() => {
-        // dispatch(getPosts(0))
-        dispatch(getBooks())
-        dispatch(getNotifications())
-    }, [])
 
     if (isLoading) return null
-    if (user) {
-        return (
-            <div id='App' key={user._id + "App"}>
-                <Sidebar />
-                <TopNavbar />
-                <div className="main-container" xs={12}>
-                    <div className='main-content'>
-                        <Routes>
-                            {/* Authentication */}
-                            <Route path='/profile' element={<Profile />}/>
-                            
-                            {/* Currently Reading */}
-                            <Route path='/' element={<CurrentlyReading />}/>
-                            <Route path='/add-book' element={<AddBookForm />}/>
-                            <Route path='/edit-book/:id' element={<EditBookForm />}/>
-        
-                            {/* Library */}
-                            <Route path='/library' exact element={<Library />}/>
-                            <Route path='/library-form/:id' exact element={<LibraryForm />}/>
-        
-                            {/* Challenge */}
-                            <Route path='/challenge' element={<Challenge />}/>
-                            <Route path='/reading-speed-test' element={<ReadingSpeedTest />}/>
-                            <Route path='/reading-speed-test/results' element={<ReadingSpeedTestCompletion />}/>
-                            <Route path='/challenge/reading-deadline' element={<ReadingDeadline />}/>
-        
-                            {/* News Feed */}
-                            <Route path='/feed' element={<Feed />} />
-                            <Route path='/public-profile/:id' element={<PublicProfile />} />
-                            <Route path='/view-post' element={<ViewPost />} />
-                            <Route path='/view-post/:id' element={<ViewPost />} />
-                        </Routes>
-                    </div>
-                </div>
-                <BottomNavbar />
-                <NotificationsModal />
-                { pointsJustScored > 0 && <PointAnimation /> }
-                {user.isNewUser && <NewUserWelcomeModal />}
+
+    if (!user) return <Routes>
+        {/* <Route path='/' element={<Auth />}/> */}
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/request-password-reset' element={<RequestPasswordReset />} />
+        <Route path='/reset-password/:token' element={<ResetPassword />} />
+    </Routes>
+
+    return <div id='App' key={user._id + "App"}>
+        <Sidebar />
+        <TopNavbar />
+        <div className="main-container" xs={12}>
+            <div className='main-content'>
+                <Routes>
+                    {/* Authentication */}
+                    <Route path='/profile' element={<Profile />}/>
+                    
+                    {/* Currently Reading */}
+                    <Route path='/' element={<CurrentlyReading />}/>
+                    <Route path='/add-book' element={<AddBookForm />}/>
+                    <Route path='/edit-book/:id' element={<EditBookForm />}/>
+
+                    {/* Library */}
+                    <Route path='/library' exact element={<Library />}/>
+                    <Route path='/library-form/:id' exact element={<LibraryForm />}/>
+
+                    {/* Challenge */}
+                    <Route path='/challenge' element={<Challenge />}/>
+                    <Route path='/reading-speed-test' element={<ReadingSpeedTest />}/>
+                    <Route path='/reading-speed-test/results' element={<ReadingSpeedTestCompletion />}/>
+                    <Route path='/challenge/reading-deadline' element={<ReadingDeadline />}/>
+
+                    {/* News Feed */}
+                    <Route path='/feed' element={<Feed />} />
+                    <Route path='/public-profile/:id' element={<PublicProfile />} />
+                    <Route path='/view-post' element={<ViewPost />} />
+                    <Route path='/view-post/:id' element={<ViewPost />} />
+                </Routes>
             </div>
-        )
-    } else {
-        return <Routes>
-            {/* <Route path='/' element={<Auth />}/> */}
-            <Route path='/' element={<LandingPage />}/>
-            <Route path='/request-password-reset' element={<RequestPasswordReset />} />
-            <Route path='/reset-password/:token' element={<ResetPassword />} />
-        </Routes>
-    }
-    
+        </div>
+        <BottomNavbar />
+        <NotificationsModal />
+        { pointsJustScored > 0 && <PointAnimation /> }
+        {user.isNewUser && <NewUserWelcomeModal />}
+    </div>
 }
 
 export default App
